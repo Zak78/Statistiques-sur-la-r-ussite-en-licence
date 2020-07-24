5b2ea6d6950134e79414171fc371aac6aec73d91
nejwaspar <58880911+nejwaspar@users.noreply.github.com>
2020-07-24 22:33:58 +0200

    upload

---
+++ models/etudiant.js
@@ -0,0 +1,11 @@
+class Etudiant{
+
+    constructor(ine, origine,semestre1,semestre2,poursuite)
+    {
+        this.ine=ine;
+        this.origine=origine;
+        this.reus_semestre1=semestre1;
+        this.reus_semestre2=semestre2;
+        this.poursuite=poursuite;
+    }
+}
